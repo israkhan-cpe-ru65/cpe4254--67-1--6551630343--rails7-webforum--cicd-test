c7c9147abc9de51b6dc1e171b8540123b303e57a
Jimmyis <jimmyis.engineerings@gmail.com>
2024-09-05 22:46:08 +0700

    feat(Forums): Added 'forum_params' strong parameters to 'Forum' controller.
    - Added 'thread' and 'controller' parameters

--- project/WebForum/app/controllers/forums_controller.rb
+++ project/WebForum/app/controllers/forums_controller.rb
@@ -5,4 +5,9 @@ class ForumsController < ApplicationController
     def new
         @forum = Forum.new
     end
+
+    private
+    def forum_params
+        params.require(:forum).permit(:thread, :content)
+    end
 end
